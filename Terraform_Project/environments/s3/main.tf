1e136da0f681fac5df7427b2211e50f5452e0165
testGit <extraksw@naver.com>
2025-05-23 19:09:57 +0900

    이미지 저장용 s3 버킷 삭제

--- Terraform_Project/environments/s3/main.tf
+++
@@ -1,3 +0,0 @@
-module "koco_s3" {
-  source = "../../modules/koco_s3"
-}
--- Terraform_Project/modules/koco_s3/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "bucket_name" {
-  description = "이미지 저장용 버킷 이름 (예: koco-s3)"
-  type        = string
-  default = "koco-img-s3"
-}
